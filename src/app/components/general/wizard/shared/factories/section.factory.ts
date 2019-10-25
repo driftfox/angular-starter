c00b7b0b3171cbb7f5a1fa64a8261e388bff9b52
Jerrol Krause <jerrolkrause@gmail.com>
2019-10-25 09:06:59 -0700

    New pattern for factory

--- src/app/components/general/wizard/shared/factories/section.factory.ts
+++ src/app/components/general/wizard/shared/factories/section.factory.ts
@@ -20,10 +20,10 @@ class SectionControl implements Wizard.SectionControl {
 
   constructor(public src: Wizard.Section, public form: FormGroup, public state: Wizard.State) {
     if (src.pages && src.pages.length) {
-      src.pages.forEach(page => this.pages[page.id] = pageControl(page, this.form, this.state));
+      src.pages.forEach(page => (this.pages[page.id] = pageControl(page, this.form, this.state)));
     }
     if (src.routes && src.routes.length) {
-      src.routes.forEach(route => this.routes[route.id] = routeControl(route));
+      src.routes.forEach(route => (this.routes[route.id] = routeControl(route)));
     }
   }
 }
@@ -34,4 +34,31 @@ class SectionControl implements Wizard.SectionControl {
  */
 export const sectionControl = (section: Wizard.Section, form: FormGroup, state: Wizard.State): Wizard.SectionControl => {
   return new SectionControl(section, form, state);
+};
+
+export const sectionControl2 = (section: Wizard.Section, form: FormGroup, state: Wizard.State): Wizard.SectionControl => {
+  // Private variables
+  const routes: Record<string, Wizard.RouteControl> = {};
+  if (section.routes && section.routes.length) {
+    section.routes.forEach(route => (routes[route.id] = routeControl(route)));
+  }
+  const pages: Record<string, Wizard.PageControl> = {};
+  if (section.pages && section.pages.length) {
+    section.pages.forEach(page => (pages[page.id] = pageControl(page, form, state)));
+  }
+  // Return
+  return {
+    ...section,
+    src: { ...section },
+    settings: {...section.settings },
+    sectionNextId: null,
+    sectionPreviousId: null,
+    slug: section.slug || section.id,
+    routes: routes,
+    pages: pages,
+    get title() {
+      return section.title;
+    },
+    // getCount: () => count.toLocaleString()
+  };
 };
